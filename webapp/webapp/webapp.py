f73000846bb99c0c19499431371abcf30905ea4b
xp6warrior <trojanowskioliver1@gmail.com>
2025-05-05 16:12:33 +0200

    Migrated from Pynecone to Reflex

--- webapp/webapp/webapp.py
+++ webapp/webapp/webapp.py
@@ -1,45 +1,39 @@
-"""Welcome to Pynecone! This file outlines the steps to create a basic app."""
-from pcconfig import config
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
 
-import pynecone as pc
+import reflex as rx
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
+from rxconfig import config
 
 
-class State(pc.State):
+class State(rx.State):
     """The app state."""
 
-    pass
+    ...
 
 
-def index() -> pc.Component:
-    return pc.fragment(
-        pc.color_mode_button(pc.color_mode_icon(), float="right"),
-        pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": pc.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex!", size="9"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
             ),
-            spacing="1.5em",
-            font_size="2em",
-            padding_top="10%",
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
         ),
+        rx.logo(),
     )
 
 
-# Add state and page to the app.
-app = pc.App(state=State)
+app = rx.App()
 app.add_page(index)
-app.compile()
