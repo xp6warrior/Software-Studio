2cdef57db0ac342a9ff3a47011bf2f1ba02d6229
sylviazska <sylviazurowska@gmail.com>
2025-05-11 15:37:51 +0200

    Create scheduler.py
    adding a scheduler that will run the matching code at 7AM and 4PM daily (times can be changes, those are the ones I think would work well with the app management)

---
+++ rte/scheduler.py
@@ -0,0 +1,18 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from matching import match_items
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def run_matching_job():
+    logging.info("running matching...")
+    match_items()
+    logging.info("matching completed")
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler(timezone='Europe/Warsaw')
+
+    scheduler.add_job(run_matching_job, 'cron', hour='7,14', minute=0)
+
+    logging.info("Scheduler started. Matching will run at 7AM and 2PM daily.")
+    scheduler.start()
