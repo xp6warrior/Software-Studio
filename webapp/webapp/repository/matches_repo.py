dc89723ac0a5d65731e6c57d93faa2c6409b6ce6
xp6warrior <trojanowskioliver1@gmail.com>
2025-06-19 12:11:14 +0200

    Remove offset/limits when selecting matches

--- webapp/webapp/repository/matches_repo.py
+++ webapp/webapp/repository/matches_repo.py
@@ -3,26 +3,20 @@ import reflex as rx
 from webapp.models2.models import Matches, Items
 from webapp.models2.enums import StatusEnum
 
-def select_matches(item: Items, offset: int = 0, limit: int = 10) -> list[Matches]:
+def select_matches(item: Items) -> list[Matches]:
     if item == None:
         raise Exception("item must not be None!")
     elif not isinstance(item, Items):
         raise Exception("item must be of type Items!")
-    if offset == None or limit == None:
-        raise Exception("offset and limit must not be None!")
-    elif type(offset) != int or type(offset) != int:
-        raise Exception("offset and limit must be of type int!")
-    elif offset < 0 or limit < offset:
-        raise Exception("offset and limit must be at least 0, and in ascending order!")
     
     with rx.session() as session:
         if item.status == StatusEnum.LOST:
             matches = session.scalars(
-                item._lost_matches.select().offset(offset).limit(limit)
+                item._lost_matches.select()
             ).all()
         elif item.status == StatusEnum.FOUND:
             matches = session.scalars(
-                item._found_matches.select().offset(offset).limit(limit)
+                item._found_matches.select()
             ).all()
     return matches
 
